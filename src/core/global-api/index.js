355517b906e50566aeb3ba44742861d921bdc8d2
Muslim <wanghuipeng@xianjincard.com>
2020-11-06 18:05:08 +0800

    Update index.js

--- src/core/global-api/index.js
+++ src/core/global-api/index.js
@@ -18,6 +18,7 @@ import {
   defineReactive
 } from '../util/index'
 
+/* 初始化全局API和一些静态方法*/
 export function initGlobalAPI (Vue: GlobalAPI) {
   // config
   const configDef = {}
@@ -34,6 +35,7 @@ export function initGlobalAPI (Vue: GlobalAPI) {
   // exposed util methods.
   // NOTE: these are not considered part of the public API - avoid relying on
   // them unless you are aware of the risk.
+  /* 暴露一些工具类方法 */
   Vue.util = {
     warn,
     extend,
@@ -46,12 +48,14 @@ export function initGlobalAPI (Vue: GlobalAPI) {
   Vue.nextTick = nextTick
 
   // 2.6 explicit observable API
+  /* 2.6新出的API，可以应对一些简单的跨组件数据状态共享的情况，相当于vuex */
   Vue.observable = <T>(obj: T): T => {
     observe(obj)
     return obj
   }
 
   Vue.options = Object.create(null)
+  /* 添加components ，directives， filters 指令组件空对象 */
   ASSET_TYPES.forEach(type => {
     Vue.options[type + 's'] = Object.create(null)
   })
@@ -59,11 +63,14 @@ export function initGlobalAPI (Vue: GlobalAPI) {
   // this is used to identify the "base" constructor to extend all plain-object
   // components with in Weex's multi-instance scenarios.
   Vue.options._base = Vue
-
+  
+  /* 合并KeepAlive中的组件对象 */
   extend(Vue.options.components, builtInComponents)
+  
 
   initUse(Vue)
   initMixin(Vue)
   initExtend(Vue)
+  /* 为vue添加静态方法，component，directive，filter*/
   initAssetRegisters(Vue)
 }
