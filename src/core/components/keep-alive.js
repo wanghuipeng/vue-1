ba26a612bf9e43b31ceeae5ed92f2cf760961dca
Muslim <wanghuipeng@xianjincard.com>
2020-11-06 17:33:41 +0800

    Update keep-alive.js

--- src/core/components/keep-alive.js
+++ src/core/components/keep-alive.js
@@ -5,10 +5,12 @@ import { getFirstComponentChild } from 'core/vdom/helpers/index'
 
 type VNodeCache = { [key: string]: ?VNode };
 
+/* 获取组件的名称 */
 function getComponentName (opts: ?VNodeComponentOptions): ?string {
   return opts && (opts.Ctor.options.name || opts.tag)
 }
 
+/* 判断pattern 中是否有name */
 function matches (pattern: string | RegExp | Array<string>, name: string): boolean {
   if (Array.isArray(pattern)) {
     return pattern.indexOf(name) > -1
@@ -34,6 +36,7 @@ function pruneCache (keepAliveInstance: any, filter: Function) {
   }
 }
 
+/* 检测缓存中的组件，如果不是当前激活的组件则销毁 */
 function pruneCacheEntry (
   cache: VNodeCache,
   key: string,
@@ -55,14 +58,14 @@ export default {
   abstract: true,
 
   props: {
-    include: patternTypes,
-    exclude: patternTypes,
+    include: patternTypes, // 设置include类型 允许[String, RegExp, Array]  缓存还没有销毁的组件
+    exclude: patternTypes, // 设置exclude类型 允许[String, RegExp, Array]   缓存已经被销毁的组件
     max: [String, Number]
   },
 
   created () {
-    this.cache = Object.create(null)
-    this.keys = []
+    this.cache = Object.create(null) //创建一个缓存的空对象
+    this.keys = [] //缓存key
   },
 
   destroyed () {
