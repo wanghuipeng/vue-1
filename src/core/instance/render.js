e0011d661da9ba2e820556f61569dbf9a5df5c7e
Muslim <wanghuipeng@xianjincard.com>
2020-11-26 14:56:09 +0800

    Update render.js

--- src/core/instance/render.js
+++ src/core/instance/render.js
@@ -98,6 +98,7 @@ export function renderMixin (Vue: Class<Component>) {
       // separately from one another. Nested component's render fns are called
       // when parent component is patched.
       currentRenderingInstance = vm
+      /* 调用render函数，返回一个VNode节点 */
       vnode = render.call(vm._renderProxy, vm.$createElement)
     } catch (e) {
       handleError(e, vm, `render`)
